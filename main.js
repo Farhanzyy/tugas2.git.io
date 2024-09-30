b23e34e98581dcf37665395c635c17df7ea3db7b
Farhanzyy <justfarhanpro@gmail.com>
2024-09-30 12:34:33 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$(document).ready(function () {
+    $('#clickButton').click(function () {
+        $('p').text('Saya Muh Farhan Mahasiswa Teknik Informatika Universitas Hasanuddin');
+    });
+});
